a8a16057764040d8514b31e57985052cb40a39e5
dragonloverlord <dragonloverlord@gmail.com>
2015-03-03 13:47:06 -0500

    added background.js

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  chrome.app.window.create(
+    'index.html',
+    {
+      id: 'mainWindow',
+      bounds: {width: 900, height: 600}
+    }
+  );
+});
